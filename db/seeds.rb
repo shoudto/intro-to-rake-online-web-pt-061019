1d9040a99bb0d12a31d9558ea6685cb99bb99b35
Oscar Shoudt <shoudto@gmail.com>
2019-09-09 17:30:47 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
 
 namespace :db do
-
+ 
   ...
-
+ 
   desc 'seed the database with some dummy data'
   task :seed do
     require_relative './db/seeds.rb'
